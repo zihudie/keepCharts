48e87fb010823f95b9779028c09e1c69d4e877b9
XIAONIU\wuyaofang <wuyaofang@xiaoniuhy.com>
2023-03-02 17:23:42 +0800

    feat:可视化

--- index.js
+++ index.js
@@ -1,58 +1,73 @@
-window.onload = function(){
-  const containerObj =  document.getElementById("container")
-  const dragObj =  document.getElementById("drag")
-  const maxHeight = containerObj.clientHeight
-  const maxWidth = containerObj.clientWidth
-  const dragHeight = dragObj.clientHeight
-  const dragWidth = dragObj.clientWidth
-  dragObj.onmousedown = function(ev){
-      const startX= ev.clientX
-      const startY= ev.clientY
-      const offsetLeft =  this.offsetLeft
-      const offestTop =  this.offsetTop
-      document.onmousemove = function(e){
-        /**
-         * 获取移动距离
-         */
-        let distanceX  =  offsetLeft + ( e.clientX -  startX) 
-        let distanceY = offestTop + ( e.clientY -  startY)  
-        
-        /**
-         * 判断边界
-         */
-        if(distanceX<0){
-          distanceX =0
-        }
-        if(distanceX + dragWidth > maxWidth) {
-          distanceX = maxWidth - dragWidth
-        }
-       
-        if(distanceY<0){
-          distanceY =0
-        }
 
-        if( distanceY + dragHeight > maxHeight) {
-           distanceY = maxHeight - dragHeight
-        }
-        /**
-         * 设置位置
-         */
-        dragObj.style.left = distanceX+ 'px'
-        dragObj.style.top = distanceY +'px'
-      }  
-  }
-  
-  document.onmouseup = function(){
-    document.onmousemove = null;
-  } 
 
-  class  Dragble {
-    constructor (){
-      
+(()=>{
+  let index  = 0
+  function getId(){
+    return ()=>{
+      return  index++
     }
-    
-
-
-    
   }
-}
+  class  Dragble {
+      constructor (objSelector,containierSelector){
+        let containerObj =  document.querySelector(containierSelector)
+        this.dragObj =  document.querySelector(objSelector)
+        
+        this.maxHeight = containerObj.clientHeight
+        this.maxWidth = containerObj.clientWidth
+        
+        this.dragHeight = this.dragObj.clientHeight
+        this.dragWidth = this.dragObj.clientWidth
+        
+        this.init()
+      }
+     getIndex(){
+      return getId()()
+     }
+     init(){
+      const _this  = this
+      this.dragObj.onmousedown = function(ev){
+        const startX= ev.clientX
+        const startY= ev.clientY
+        const offsetLeft = this.offsetLeft
+        const offestTop = this.offsetTop
+        _this.dragObj.style.zIndex = _this.getIndex()
+  
+        document.onmousemove = function(e){
+          /**
+           * 获取移动距离
+           */
+          let distanceX  =  offsetLeft + ( e.clientX -  startX) 
+          let distanceY = offestTop + ( e.clientY -  startY)  
+          
+          /**
+           * 判断边界
+           */
+          if(distanceX < 0){
+            distanceX = 0
+          }
+          if(distanceX + _this.dragWidth > _this.maxWidth) {
+            distanceX = _this.maxWidth - _this.dragWidth
+          }
+         
+          if(distanceY < 0){
+            distanceY = 0
+          }
+  
+          if( distanceY + _this.dragHeight > _this.maxHeight) {
+             distanceY = _this.maxHeight - _this.dragHeight
+          }
+          /**
+           * 设置位置
+           */
+          _this.dragObj.style.left = distanceX+ 'px'
+          _this.dragObj.style.top = distanceY +'px'
+        }  
+  
+      }
+      document.onmouseup = function(){
+        document.onmousemove = null;
+      } 
+     }
+  
+  }
+})()
